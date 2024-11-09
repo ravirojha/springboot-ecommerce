8b9af92ab6d0fbd78f4d82cc22058923f2efeb88
Ravi Ranjan  Ojha <raviranjanojha999@gmail.com>
2024-11-10 03:28:29 +0530

    order microservice config

--- services/product/src/main/java/com/example/ecommerce/product/ProductController.java
+++ services/product/src/main/java/com/example/ecommerce/product/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/ap/v1/product")
+@RequestMapping("/ap/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService service;
