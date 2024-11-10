0c39d9de87b0371ee88a7b02477208cfe3b898f0
Ravi Ranjan  Ojha <raviranjanojha999@gmail.com>
2024-11-11 02:46:58 +0530

    notification service completed

---
+++ services/notification/src/main/java/com/ravi/ecommerce/kafka/payment/PaymentMethod.java
@@ -0,0 +1,9 @@
+package com.ravi.ecommerce.kafka.payment;
+
+public enum PaymentMethod {
+    PAYPAL,
+    CREDIT_CARD,
+    VISA,
+    MASTER_CARD,
+    BITCOIN
+}
---
+++ services/notification/src/main/java/com/ravi/ecommerce/notification/Notification.java
@@ -0,0 +1,25 @@
+package com.ravi.ecommerce.notification;
+
+import com.ravi.ecommerce.kafka.order.OrderConfirmation;
+import com.ravi.ecommerce.kafka.payment.PaymentConfirmation;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document
+public class Notification {
+
+    @Id
+    private String id;
+    private NotificationType type;
+    private LocalDateTime notificationDate;
+    private OrderConfirmation orderConfirmation;
+    private PaymentConfirmation paymentConfirmation;
+}
